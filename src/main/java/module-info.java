918d1d47f8eae6b8e184dc0994bcc70c75d0ea08
AimeurAbderrahim <232332015002@etu.univ-usto.dz>
2025-04-27 18:44:04 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module testpackage.gestiondinventaireyrm {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens testpackage.gestiondinventaireyrm to javafx.fxml;
+    exports testpackage.gestiondinventaireyrm;
+}
